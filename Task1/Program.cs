509c60bf4094c869a81e3b03572f6e786bb5e525
Roman <ramec154@mail.ru>
2022-08-11 01:41:03 +0300

    DZ6

---
+++ Task1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+Console.WriteLine("Введите количество чисел");
+int x = Convert.ToInt32(Console.ReadLine());
+int n = 0;
+
+int y = 0;
+for (int i = 0; i < x; i++)
+{
+    Console.WriteLine("Введите число");
+    y = Convert.ToInt32(Console.ReadLine());
+    if (y > 0)
+        n++;
+}
+Console.WriteLine(n);
